7a7c20c867d090d3850708a2c1644b0f859a33b3
pcmendes1973 <pcmendes1973@users.noreply.github.com>
2015-10-08 11:16:29 -0300

    Update Project Euler 29.lisp
    Added bunny ears to parameter

--- Project Euler 29.lisp
+++ Project Euler 29.lisp
@@ -3,14 +3,14 @@
 ;;;; Paulo Mendes, 8-OCT-2015
 
 
-(defparameter nums '())
+(defparameter *nums* '())
 
-;;; Builds a list nums of all a^b where a in [2,100] and b in [2,100]
+;;; Builds a list *nums* of all a^b where a in [2,100] and b in [2,100]
 (loop for i from 2 to 100 do
-   (loop for j from 2 to 100 do (setf nums (cons (expt i j) nums))))
+   (loop for j from 2 to 100 do (setf *nums* (cons (expt i j) *nums*))))
 
 ;;; Calculates length after removing duplicates
-(length (remove-duplicates nums))
+(length (remove-duplicates *nums*))
 
 ;;; Solution in a single line, just for kicks
 (length (remove-duplicates (loop for n from 0 to 9701 collect (expt (+ 2 (floor n 98)) (+ 2 (mod n 99))))))
