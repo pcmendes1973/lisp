dd8e78f7623a0346aba1b797d50e93ce3c9662fd
pcmendes1973 <pcmendes1973@users.noreply.github.com>
2015-10-08 02:14:08 -0300

    Create Project Euler 29.lisp
    Solution to problem 29 in Project Euler - https://projecteuler.net/problem=29

---
+++ Project Euler 29.lisp
@@ -0,0 +1,13 @@
+;;;; Solution to problem 29 in Project Euler
+;;;; https://projecteuler.net/problem=29
+;;;; Paulo Mendes, 8-OCT-2015
+
+
+(defparameter nums '())
+
+;;; Builds a list nums of all a^b where a in [2,100] and b in [2,100]
+(loop for i from 2 to 100 do
+   (loop for j from 2 to 100 do (setf nums (cons (expt i j) nums))))
+
+;;; Calculates length after removing duplicates
+(length (remove-duplicates nums))
