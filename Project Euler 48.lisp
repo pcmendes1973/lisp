8829fcf601e35ef4d4c6943aedd070d43eb7d228
pcmendes1973 <pcmendes1973@users.noreply.github.com>
2015-10-10 16:44:35 -0300

    Update Project Euler 48.lisp

--- Project Euler 48.lisp
+++ Project Euler 48.lisp
@@ -1,5 +1,5 @@
 ;;;; Solution for problem 48 in Project Euler
-;;;; https://projecteuler.net/problem=14
+;;;; https://projecteuler.net/problem=48
 ;;;; Paulo Mendes, 10-OCT-2015
 
 (/ (loop for 1 from 1 to 1000 summing (expt i i)) 10000000000)
