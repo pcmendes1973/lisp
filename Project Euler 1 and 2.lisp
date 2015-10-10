522a443113a25bd54a1e09d54fc547cd3f9c7d77
pcmendes1973 <pcmendes1973@users.noreply.github.com>
2015-10-10 04:09:42 -0300

    Update Project Euler 1 and 2.lisp

--- Project Euler 1 and 2.lisp
+++ Project Euler 1 and 2.lisp
@@ -1,10 +1,12 @@
-;;;; Project Euler Problem 1
-;;;; https://projecteuler.net/problem=1
+;;;; Project Euler Problems 1 and 2
+;;;; https://projecteuler.net/problem=1 and https://projecteuler.net/problem=2
+;;;; Paulo Mendes, 10-OCT-2015
+
+;;; Problem 1
 (loop for i from 1 to 1000
     summing (if (or (= (mod i 3) 0) (= (mod i 5) 0)) i 0))
 
-;;;; Project Euler Problem 2
-;;;; https://projecteuler.net/problem=2
+;;; Problem 2
 (loop for i = 0 then j
       and j = 1 then (+ i j)
       until (> j 4000000)
