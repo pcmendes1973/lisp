fb6888230f6d8593314c0b3ebecde3037562163e
pcmendes1973 <pcmendes1973@users.noreply.github.com>
2015-11-07 10:43:52 -0200

    Update Project Euler 36.lisp
    New solution that actually works

--- Project Euler 36.lisp
+++ Project Euler 36.lisp
@@ -2,27 +2,22 @@
 ;;;; https://projecteuler.net/problem=36
 ;;;; Paulo Mendes, 03-NOV-2015
 
-(defun digits (n)
+(defun digits (n &optional (base 10))
 "Returns the digits of n in a list"
-  (if (< n 10)
+  (if (< n base)
     (list n)
    (multiple-value-bind (div mod)
-      (floor n 10)
-      (append (digits div) (list mod)))))
+      (floor n base)
+      `(,@(digits div base) ,mod))))
 
-(defun to-binary (n)
-"Returns the binary digits of n in a list"
-   (if (< n 2)
-     (list n)
-    (append (to-binary (floor n 2)) (list (logand n 1)))))
-    
 (defun is-palindrome? (lst)
-"t is lst is a palindrome, F otherwise"
+"T is lst is a palindrome, Nil otherwise"
   (if (> (length lst) 1)
     (and (= (car lst) (car (last lst)))
          (is-palindrome? (cdr (butlast lst))))
+   T))
 
 ;; Sums all numbers having palindrome digits in both base 2 and base 10
-(loop for i from 10 to 1000000
-      if (and (is-palindrome? (digits i))
-              (is-palindrome? (to-binary i))) summing i)
+(loop for n from 1 to 1000000
+      if (and (is-palindrome? (digits n))
+              (is-palindrome? (digits n 2))) summing n)
