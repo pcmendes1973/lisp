17370d41c3118e998c444ad807ad30753209b360
pcmendes1973 <pcmendes1973@users.noreply.github.com>
2015-11-07 11:39:14 -0200

    Update Project Euler 7.lisp
    New solution that actually works

--- Project Euler 7.lisp
+++ Project Euler 7.lisp
@@ -2,19 +2,19 @@
 ;;;; https://projecteuler.net/problem=7
 ;;;; Paulo Mendes, 11-OCT-2015
 
-(defun nth-prime (prime-to-find)
-"Finds nth prime number"
-  (if (< prime-to-find 3) (1+ prime-to-find))
-  (let* ((primes '(2 3))         ; primes: Primes found so far
-         (last-prime primes))    ; last-prime: End of the primes list
-  (loop for i = 6 then (if (> j 0) (+ i 6) i)
-        and j = -1 then (* j -1)
-        for k = (+ i j)
-        for is-prime = (notany #'(lambda (x) (= (mod k x) 0)) primes) ; Checks if k is prime
-        for n-primes = 3 then (if is-prime (1+ n-primes) n-primes)    ; Updates prime counter if it is
-        if is-prime do (setf (cdr last-prime) (cons k Nil)            ; Appends k to primes list as needed
-                             last-prime (cdr last-prime))
-        until (= n-primes prime-to-find)
-        finally (return i))))
+(let* ((sieve '((5 5))) (sieve-end sieve))
+  (defun caught-by-elem (elem n)
+    (let ((i (car elem)))
+      (or (> i n) (setf (car elem) (+ i (cadr elem))))
+      (= i n)))
 
-(nth-prime 10001)
+(defun nth-prime (n) 
+  (loop for i = 6 then (if (> j 0) (+ i 6) i)
+        for j = 1 then (* j -1)
+        for k = (+ i j)
+        for is-prime = (notany #'(lambda (x) (caught-by-elem x k)) sieve)
+        counting is-prime into n-primes
+        if is-prime do (setf (cdr sieve-end) `((,k ,k))
+                              sieve-end (cdr sieve-end))
+        until (= n-primes (- n 2))
+        finally (return (cadar (last sieve 2))))))
