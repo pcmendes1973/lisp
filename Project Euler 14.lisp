eb1f7a4d410a012ec36c3826cbdab4be5731bfe6
pcmendes1973 <pcmendes1973@users.noreply.github.com>
2015-11-07 07:44:17 -0200

    Update Project Euler 14.lisp
    Put all code in a single closure and eliminated expensive call to gethash

--- Project Euler 14.lisp
+++ Project Euler 14.lisp
@@ -2,23 +2,22 @@
 ;;;; https://projecteuler.net/problem=14
 ;;;; Paulo Mendes, 8-OCT-2015
 
-(defparameter *collatz* (make-hash-table))
-(setf (gethash 1 *collatz*) 1)
+(let ((longest-seq 0)
+      (max-values 0)
+      (collatz (make-hash-table :size 3000000)))
 
 (defun n-collatz (n)
-"""Returns the number of steps to reach 1 following the Collatz sequence.
-    Memo-ized using hash table *collatz*
-    Entry 1 -> 1 serves as stopping criterion for recursive function"""
-   (or (gethash n *collatz*)
-       (setf (gethash n *collatz*)
+"Returns the number of steps in Collatz sequence n
+ Memoized using hash table collatz "
+   (or (gethash n collatz)        ; Entry 1 -> 1 serves as stopping criterion for recursion  
+       (setf (gethash n collatz)
          (if (evenp n)
             (1+ (n-collatz (/ n 2)))
             (1+ (n-collatz (1+ (* n 3))))))))
-            
-;;; Calculates the maximum value of n-collatz in interval [1, 1000000]            
-(let ((max-steps 0) (longest-seq 0))
-  (loop for i from 1 to 1000000 maximize (n-collatz i))
-  (maphash #'(lambda (k v) (if (> v max-steps)
-                             (setf max-steps v
-                                   longest-seq k))) *collatz*)
-(values longest-seq))
+
+  (setf (gethash 1 collatz) 1)
+  (loop for i from 1 to 1000000
+        for j = (n-collatz i)
+        if (> j longest-seq) do (setf longest-seq j
+                                      max-values i))
+  (values longest-seq max-values))
