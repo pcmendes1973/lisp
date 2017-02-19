9caafc17a4fd45a1587271ee90335932d7c07481
Paulo Mendes <pcmendes1973@users.noreply.github.com>
2017-02-19 07:06:23 -0300

    Update Project Euler 43.lisp
    Incorporated  input from http://stackoverflow.com/questions/42303306/what-is-the-best-way-of-combining-key-and-rest-in-a-lisp-macros-lambda-list/42303535#42303535

--- Project Euler 43.lisp
+++ Project Euler 43.lisp
@@ -1,30 +1,33 @@
 ;;;; Solution to Problem 43 in Project Euler
-;;;; Paulo Mendes, 15-FEB-2017
+;;;; Paulo Mendes, 15-FEB-2017 
 
-;;; Anaphoric version
-;;; To make this macro non-anaphoric, change (,var ,vec) to (,var (copy-seq ,vec))
-(defmacro run-permutations (var vec &rest body)
-"Uses Heap's algorithms to generate all permutations of sequence vec,
- puts the results in variable var and runs code in body for each
- permutation"
-  `(let ((,var ,vec))
-     (labels ((generate (&optional (n (length ,var)))
-       (if (= n 1)
-         (progn ,@body)
-        (progn 
-           (loop for i from 0 below (1- n)
-                 do (progn 
-                      (generate (1- n))
-                      (rotatef (aref ,var (if (evenp n) i 0))
-                               (aref ,var (1- n)))))
-           (generate (1- n))))))
-           
+(defmacro do-permutations ((var vec &key anaphoric (len (length vec))) &body body)
+ "Generates all permutations of vec, puts results into var and executes body for 
+  each permutation.
+   KEYS:
+     anaphoric: if defined, modifies var outside the macro, preserves it otherwise
+     len: number of items that will be permuted, default is the full vector"
+  (let ((n (gensym)))
+  `(let ((,var ,(if anaphoric vec `(copy-seq ,vec))))
+     (labels ((generate (&optional (,n ,len))
+                (if (= ,n 1)
+                    (progn ,@body)
+                  (let ((n-1 (1- ,n)))
+                    (loop for i from 0 below n-1
+                          do (progn 
+                               (generate n-1)
+                               (rotatef (aref ,var (if (evenp ,n) i 0))
+                                        (aref ,var n-1))))
+                    (generate n-1)))))
+       (generate))))))
+
+         
  ;;; Uses the run-permutations macro to generate all possible permutations
  ;;; of 10 digits and tests them against a sequence of closures within an
  ;;; and macro.
  (defmacro euler43 ()
    `(let* ((sum-total 0))
-        (run-permutations num #(0 1 2 3 4 5 6 7 8 9)
+        (do-permutations num #(0 1 2 3 4 5 6 7 8 9)
            (and ,@(loop for i in '(17 13 11 7 5 3 2)
                         for j from 7 downto 1
                         collect `(zerop (mod (+ (* 100 (aref num ,j))
