39c4d0f6ec1048502ecadd26d91f6856204d9e6d
Paulo Mendes <pcmendes1973@users.noreply.github.com>
2017-05-30 00:55:35 -0300

    Create Project Euler 116.lisp

---
+++ Project Euler 116.lisp
@@ -0,0 +1,20 @@
+;;;; Solution for problem 116 in Project Euler
+;;;; Paulo Mendes, 30-MAY-2017
+
+
+(defun sum-digits (n)
+   (if (zerop n) 0
+      (multiple-value-bind (mod rest)
+         (floor n 10)
+            (+ (sum-digits mod) rest))))
+
+(defun is-a? (n)
+   (loop for i from 2 to 20
+         for num = (expt n 2) then (* num n)
+         when (= (sum-digits num) n)
+           collect num))
+
+(nth 29 (sort (loop for i from 2 to 100
+                    when (is-a? i)
+                      nconc it)
+              #'<))
